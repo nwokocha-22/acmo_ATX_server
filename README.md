553e3afba7624f979b2c91428696898b8d142a1a
Nwokocha22 <nwokocha.algoscience@gmail.com>
2023-01-09 19:57:24 +0200

    created the readme.md file

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## ACTIVITY MONITOR
+
+# Keyboard, Mouse, and Clipboard Activity Monitoring
+This scripts monitors the user's keyboard, mouse, and clipboard activities. It captures the number of keystroke, and mouse moves every 10 minutes and logs the information to a remote server through a socket.
+
+# Video Capturing
+The activity monitor script also consist of a helper module that captures the screenshot of the user's screen and transmits the video via UDP sockets to a remote server where it can be live streamed and also saved in a video file in a designated folder for future references. A new folder where the video is saved is created using the client's ip address if it does not already exist.
+
+# Copy Policy
+If the user copies content more than 500 KB per hour or equal or more than 1500 KB for 24 hours, an email notification is sent to the administrator. the email message includes the ip address of the client, the date and time of occurence, the size of the file copied, the actual text copied (for text files), or attachment of the file(s) for binary files.
+
+## AUTHOR: Maru Koch
--- helpers/videoServer.py
+++ helpers/videoServer.py
@@ -39,7 +39,7 @@ class ReceiveVideo:
 					sock.close()
 					break
 
-	def recv_data(self, client_ip):
+	def recv_data(self, client_ip, video_file):
 		# create a window with the with title of the client ip 
 		print("receiving data...")
 		cv2.namedWindow(client_ip, cv2.WINDOW_NORMAL)
@@ -47,27 +47,29 @@ class ReceiveVideo:
 		with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock_udp:
 			sock_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			sock_udp.bind(('', self.port))
-
 			while True:
 				packet, addr = sock_udp.recvfrom(self.BUFFER) # 1 MB buffer
 				
 				#: stream the data from that particular client
 				if client_ip == addr[0]:
-					img = cv2.imdecode(np.frombuffer(packet, np.uint8), cv2.IMREAD_COLOR)
+					frame = cv2.imdecode(np.frombuffer(packet, np.uint8), cv2.IMREAD_COLOR)
 					title = f'{client_ip if addr else "VIDEO"}'
 
-					#. write to video File:
-
-					cv2.imshow(title, img)
+					#. write frame to video File:
+					video_file.write(frame)
+					cv2.imshow(title, frame)
 					key = cv2.waitKey(1) & 0xFF
 
 				if key == ord('q'):
 					sock_udp.close()
 					break
-
+				
 		# thread = Thread(target=processImg, args=(sock_udp, addrs[0]))
 		# thread.start()
-	
+
+		#: release the video file
+		video_file.release()
+
 	def connect(self):
 		"""
 		establishes a three way hand shake with the clients, and spawn a thread to send data
@@ -84,29 +86,29 @@ class ReceiveVideo:
 				print("waiting for a new connection")
 				client, addr = sock_tcp.accept()
 				print("connected to:", client)
+
 				#: create_dir(addr[0])
-				file_path = self.create_dir(client_ip, self.date.month)
+				filepath = self.create_dir(addr[0], self.date.month, self.filename)
 				data = client.recv(1024).decode()
+
 				print("message from client:", data)
 				if data == "ready":
 					client.send(str.encode("shoot"))
-					thread = Thread(target=self.recv_data, args=(addr,))
+					thread = Thread(target=self.recv_data, args=(addr, filepath))
 					thread.start()
 					print("exited thread")
 
-	def create_dir(self, ip, month, filename):
+	def create_dir(self, client_ip, month, filename):
 		#: if video recoring folder exists, open it, else create it
 		#: if existing folder for the current month, open it, else create it
 		#: if video_recoding for today, open it, else create it
 		#: write streamed frame to video_file
 
-		path = Path("C:\Recorded Videos")
-	
+		path = Path("C:\Recorded Videos")/client_ip/month/f'{self.date}-screen-recording.mkv'
 		#: check if Recorded Videos folder exists in the local drive dir
 		if not path.exists():
-			dir  = path/ip/month/filename
 			return Path.mkdir(dir)
-			
+
 		else:
 			#: if the folder exits, navigate to it.
 			#: if there is a folder for the client, navigate to it
