f5b321fd97374469df246f27ed2db260fed7eeac
Nwokocha22 <nwokocha.algoscience@gmail.com>
2023-01-17 12:55:55 +0200

    created a service executable file

--- service.py
+++ service.py
@@ -1,68 +1,90 @@
-import win32serviceutil
-import win32event
 import subprocess
+import win32evtlog
+import win32evtlogutil
+import win32con
+import win32event
+import win32service
+import win32serviceutil
 
-#: INSTALL SERVICE
-#: python path/to/service.py install
-
-#: START SERVICE
-#: net start ActivityMonitorService
-
-#: STOP SERVICE
-#: net stop ActivityMonitorService
-
-#: REMOVE SERVICE
-# sc delete servicename
-
-# 1072 error: means service is still there but marked for delection
-
-"""
-
-Anyone facing this issue, just copy pywintypes36.dll
-
-from Python36\Lib\site-packages\pywin32_system32
-
-to Python36\Lib\site-packages\win32
-
-Helpful commands:
-
-"""
-
-#: Install a service: python scriptName.py install
-
-#: Uninstall a service: python scriptName.py remove
-
-#: Start a service: python scriptName.py start
-
-#: Update service: python scriptName.py update
-
-
-
-class ActivityMonitorService(win32serviceutil.ServiceFramework):
+class LogonLogoffService(win32serviceutil.ServiceFramework):
+    """
+    A windows service that listens for remote desktop logon and logoff events in real-time
+    and starts/kills a python script when a logon/logoff event is detected
+    """
     _svc_name_ = "ActivyMonitorService"
     _svc_display_name_ = "Activity Monitor Service"
     _svc_description_ = "Starts the activity monitor script when a user is log in"
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        self.stop_event = win32event.CreateEvent(None, 0, 0, None)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
         self.process = None
 
-    def SvcStop(self):
-        self.stop()
-        win32event.SetEvent(self.stop_event)
-
     def SvcDoRun(self):
-        self.start()
-        #: Listen for Login 
-        win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
+        """
+        Service initialization code
+        """
+        # Report service status as running
+        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+        # Log service start event
+        win32evtlogutil.ReportEvent(self._svc_name_,
+                                    win32evtlog.EVENTLOG_INFORMATION_TYPE,
+                                    0, # eventCategory
+                                    win32service.SERVICE_STARTED, # eventID
+                                    None, # userSid
+                                    [self._svc_name_], # strings
+                                    None) # data
+        # Continuously listen for logon and logoff events
+        while 1:
+            # Check for service stop event
+            rc = win32event.WaitForSingleObject(self.hWaitStop, win32event.INFINITE)
+            if rc == win32event.WAIT_OBJECT_0:
+                # Log service stop event
+                win32evtlogutil.ReportEvent(self._svc_name_,
+                                    win32evtlog.EVENTLOG_INFORMATION_TYPE,
+                                    0, # eventCategory
+                                    win32service.SERVICE_STOPPED, # eventID
+                                    None, # userSid
+                                    [self._svc_name_], # strings
+                                    None) # data
+                break
+            # Open the Security event log in real-time
+            event_log = "Security"
+            event_source = None
+            h = win32evtlog.OpenEventLog(None, event_log)
+            flags = win32evtlog.EVENTLOG_FORWARDS_READ|win32evtlog.EVENTLOG_SEEK_READ
+            last_record = win32evtlog.GetNumberOfEventLogRecords(h)
+            process = None
+            while True:
+                events = win32evtlog.ReadEventLog(h, flags, 0)
+                if events:
+                    for event in events:
+                        if event.EventType == win32con.EVENTLOG_AUDIT_SUCCESS:
+                            if event.EventID == win32con.EVENTLOG_AUDIT_SUCCESS:
+                                if event.StringInserts[-1] == "Logon":
+                                    print("Remote desktop logon event detected")
+                                    script_path = r"C:\ActivityMonitor\core.py"
+                                    args = ["arg1", "arg2", "arg3"]
+                                    # Start the script and pass arguments
+                                    process = subprocess.Popen(script_path, shell=True)
+                                elif event.StringInserts[-1] == "Logoff":
+                                    print("Remote desktop logoff event detected")
+                                    # Kill the script when logoff event is detected
+                                    if process:
+                                        process.kill()
+                                    current_record = win32evtlog.GetNumberOfEventLogRecords(h)
+                                    if current_record != last_record:
+                                        last_record = current_record
+                                    else:
+                                    # Wait for a second before checking again
+                                        win32event.WaitForSingleObject(self.hWaitStop, 1000)
 
-    def start(self):
-        self.process = subprocess.Popen("./core.py", shell=True)
+    def SvcStop(self):
+        """
+        Stops the service.
+        """
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
 
-    def stop(self):
-        if self.process:
-            self.process.terminate()
-
-if __name__ == '__main__':
-    win32serviceutil.HandleCommandLine(ActivityMonitorService)
+if __name__ == "__main__":
+    win32serviceutil.HandleCommandLine(LogonLogoffService)
