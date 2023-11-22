62932ba8f0e8ecc6104552335ee8769d191cacd4
nwokocha-22 <joshtem6@gmail.com>
2023-11-22 17:03:43 +0200

    fixed error in collecting client IP

--- helpers/logServer.py
+++ helpers/logServer.py
@@ -32,7 +32,7 @@ class LogRecordStreamHandler(socketserver.StreamRequestHandler):
                 self.logRecord(record)
             except ConnectionResetError:
                 if not self.logged:
-                    client_ip =  self.client_address
+                    client_ip =  self.client_address[0]
                     trace = f"Logs: {client_ip} disconnected"
                     error_logger.info(trace)
                     self.logged = True
@@ -93,7 +93,7 @@ class LogRecordStreamHandler(socketserver.StreamRequestHandler):
         record: str
             Log file received from client
         """
-        client_ip =  self.client_address
+        client_ip =  self.client_address[0]
         path = self.create_dir(client_ip)
 
         if not path.exists():
